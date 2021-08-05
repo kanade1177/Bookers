db8a852bd4380788a18509ed14566ae0ef29c255
EC2 Default User <[abkeybg111177@gmail.com]>
2021-08-05 06:56:25 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all.order(created_at: :desc)
+    @books = Book.all
     @book = Book.new
   end
 
@@ -17,7 +17,7 @@ class BooksController < ApplicationController
     if @book.save
      redirect_to book_path(@book.id), notice: "Book was successfully created."
     else
-     @books = Book.all.order(created_at: :desc)
+     @books = Book.all
      render :index
     end
   end
